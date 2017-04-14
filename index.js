22d1ad3b7dde3e7056f02c2fd0038f672503a327
Laza Bogdan <laza.bogdan@macro-net.ro>
2017-04-14 15:50:17 +0300

    add cache

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 const async = require('async')
-const path = require('path')
 const postcss = require('postcss')
+const RawSource = require('webpack-sources').RawSource
+const merge  = require('lodash.merge')
+const Cache = require('fs-simple-cache')
 
-const WebpackRTLWrapPlugin = function(options = {}) {
-  this.options = options
-}
+class WebpackRTLWrapPlugin {
+  constructor (options = {}) {
+    this.options = merge({
+      testCss: /\.css?$/,
+      testIsRTL: /\.rtl\.css?$/
+    }, options)
+  }
 
-WebpackRTLWrapPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', (compilation, callback) => {
-    async.eachOfLimit(compilation.chunks, 5, (chunk, key, cb) => {
-      let promise = Promise.resolve()
+  apply (compiler) {
+    const cache = new Cache(this.options)
+    compiler.plugin('emit', (compilation, callback) => {
+      async.eachOfLimit(compilation.chunks, 5, (chunk, key, cb) => {
+        let promise = Promise.resolve()
 
-      chunk.files.forEach((file) => {
-        const asset = compilation.assets[file]
-        const isRTL = file.indexOf('.rtl.css') !== -1
-        if (path.extname(file) === '.css') {
-          promise = promise.then(() => { 
-            return postcss([
-              require('postcss-wrap')({ selector: '[dir=' + (isRTL ? 'rtl' : 'ltr') + ']', skip: /^html/ })
-            ])
-            .process(asset.source(), {}).then(function (result) {
-              asset.source = () => result.css
-              asset.size = () => result.css.length
+        chunk.files.forEach((file) => {
+          const asset = compilation.assets[file]
+          const isRTL = this.options.testIsRTL.test(file)
+          
+          if (this.options.testCss.test(file)) {
+            const source = asset.source()
+            let content
+            if (content = cache.get(source).content) {
+              promise = promise.then(() => content)
+            }
+            else {
+              promise = promise.then(() => { 
+                return postcss([
+                  require('postcss-wrap')({ selector: '[dir=' + (isRTL ? 'rtl' : 'ltr') + ']', skip: /^html/ })
+                ])
+                .process(source, {}).then(function (result) {
+                  cache.put(source, { content: result.css })
+                  return result.css
+                })
+              })
+            }
+            promise = promise.then(result => {
+              compilation.assets[file] = new RawSource(result)
             })
-          })
-        }
-      })
+          }
+        })
 
-      promise.then(() => cb())
-    }, callback)
-  })
+        promise.then(() => cb())
+      }, callback)
+    })
+  }
 }
 
 module.exports = WebpackRTLWrapPlugin
